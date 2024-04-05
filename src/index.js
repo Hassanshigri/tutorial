82780fd5b8fe9acbeabf384d49ff7139d066da0e
Muhammad Hassan <hassanshigri435@gmail.com>
2024-04-05 16:41:20 +0500

    First challenge

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//we can also render jsx elements in array
-//React fragment use to avoid double div problems 
-//we can also use " <> </> " instead of react.fragment
+//Create a Simple Web App on Top 5 Netflix Series List using JSX
 ReactDOM.render(
-<React.Fragment>
-    <h1>Muhammad hassan</h1>
-    <p>I am 22 years old</p>
-    <h2>Habbits</h2>
-    <p>Coding,Reading</p>
-</React.Fragment>,
+<>
+    <h1>Hassan Fav Netflix Series list</h1>
+    <p>Following are my Fav series</p>
+    <ol>
+        <li>A</li>
+        <li>B</li>
+        <li>C</li>
+        <li>D</li>
+        <li>E</li>
+    </ol>
+</>,
     document.getElementById("root"));
