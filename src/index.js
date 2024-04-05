3dbad7046fc591992bb856cf5e2621a01acbace4
Muhammad Hassan <hassanshigri435@gmail.com>
2024-04-05 17:06:20 +0500

    React Expressions

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//Create a Simple Web App on Top 5 Netflix Series List using JSX
+const name ="Muhammad Hassan"
 ReactDOM.render(
 <>
-    <h1>Hassan Fav Netflix Series list</h1>
-    <p>Following are my Fav series</p>
-    <ol>
-        <li>A</li>
-        <li>B</li>
-        <li>C</li>
-        <li>D</li>
-        <li>E</li>
-    </ol>
+  <h1>React expression</h1>
+  <p>My name is {name}</p>
+  <p>My lucky number is {Math.random()}</p>
 </>,
     document.getElementById("root"));
