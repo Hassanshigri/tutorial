d7b6806a68b0db23f4c267d01719f3d92c8befbd
Muhammad Hassan <hassanshigri435@gmail.com>
2024-04-05 15:56:43 +0500

    multipule JSX element to render

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-// var React = require("react");
 import React from "react";
-// var ReactDOM = require("react-dom");
-import  ReactDOM from "react";
-
-ReactDOM.render(<h1>Hello World</h1>,document.getElementById("root"));
+import ReactDOM from "react-dom";
+//we can also render jsx elements in array
+ReactDOM.render(
+[
+    <h1>Muhammad hassan</h1>,
+    <p>I am 22 years old</p>,
+    <h2>Habbits</h2>,
+    <p>Coding,Reading</p>
+],
+    document.getElementById("root"));
