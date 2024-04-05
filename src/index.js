40945a81acecbf16732b4baeb8a65c77da7aaac7
Muhammad Hassan <hassanshigri435@gmail.com>
2024-04-05 16:19:37 +0500

    application of react fragment

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 //we can also render jsx elements in array
+//React fragment use to avoid double div problems 
+//we can also use " <> </> " instead of react.fragment
 ReactDOM.render(
-[
-    <h1>Muhammad hassan</h1>,
-    <p>I am 22 years old</p>,
-    <h2>Habbits</h2>,
+<React.Fragment>
+    <h1>Muhammad hassan</h1>
+    <p>I am 22 years old</p>
+    <h2>Habbits</h2>
     <p>Coding,Reading</p>
-],
+</React.Fragment>,
     document.getElementById("root"));
